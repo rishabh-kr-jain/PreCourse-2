16399eb4b3750b0616c210e171932fb6f020cd00
Rishabh Kumar Jain <rishabh803@gmail.com>
2024-12-21 15:57:04 -0800

    mid point of linked list

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,45 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data= data
+        self.next= None  
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
+        newNode = Node(new_data)
+        newNode.next= self.head
+        self.head= newNode
+
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        if (self.head == None):
+            return
+        len_node = self.head
+        len=0
+        while (len_node is not None):
+            len_node= len_node.next
+            len = len + 1
+        # print("total length", len)
+        mid= len//2
+        # print("mid", mid)
+        mid_node = self.head
+        # print('head',self.head.data)
+        while ( mid):
+            mid_node = mid_node.next
+            mid = mid - 1
+        print(mid_node.data)
+        return mid_node.data
+
+            
+
 
 # Driver code 
 list1 = LinkedList() 
