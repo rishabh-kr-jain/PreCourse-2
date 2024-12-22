0b547c76051bd7d4181c1639f19f60d89e5a75a8
Rishabh Kumar Jain <rishabh803@gmail.com>
2024-12-21 20:57:01 -0800

    quick sort

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,13 +2,31 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+    #selecting highest element as pivot
+    pivot= arr[high]
+    # print('printing the pivot element',pivot)
+    #initial index
+    i= low-1
+
+    for j in range(low,high):
+        if(arr[j]<pivot):
+            # initial index of i
+            i+=1
+            arr[i], arr[j]=arr[j], arr[i]
+
+    arr[i+1], arr[high]=arr[high], arr[i+1]
+    return i+1
   
-  
-    #write your code here
+
   
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,low,high):
+    if(low<high):
+        pivot_index= partition(arr,low,high)
+        quickSort(arr, low,pivot_index-1)
+        quickSort(arr, pivot_index+1, high)
+
     
     #write your code here
   
